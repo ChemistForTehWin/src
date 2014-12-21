ee05e9efd13c2b07522572802922992187b28b5b
ChemistFTW <nethrezim@mail.ru>
2014-12-21 18:55:04 +0900

    let

--- com/ChemistFTW/BardsAdventures/AppWindow.java
+++ com/ChemistFTW/BardsAdventures/AppWindow.java
@@ -1,12 +1,17 @@
 package com.ChemistFTW.BardsAdventures;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AppWindow extends JFrame {
     public AppWindow() {
         super("Bard's Adventures v.0"); // !Window header, should export from version stamp (set in init())!
         setBounds(50, 50, 1024, 768); //    !CHANGE THIS TO EXPORT FROM INIT VALUES!
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setIconImage(getToolkit().getImage(getClass().getResource("img/bardimage.jpg")));
+        setBackground(Color.black);
+        setForeground(Color.black);
     }
 }
 
--- com/ChemistFTW/BardsAdventures/Application.java
+++ com/ChemistFTW/BardsAdventures/Application.java
@@ -1,14 +1,14 @@
 package com.ChemistFTW.BardsAdventures;
 
+
 public class Application {
     public static CustomEventHandler customEventHandler = new CustomEventHandler();
     public static GameTickHandler tickHandler = new GameTickHandler();
-
+    public static Scripts scripts = new Scripts();
     public static void start() {
 
         AppWindow appWindow = new AppWindow();
         appWindow.setVisible(true);
-        Scripts scripts = new Scripts();
         scripts.RunAll();
         tickHandler.isGamePaused = false;
         tickHandler.GameTickHandlerStart();
@@ -17,7 +17,9 @@ public class Application {
     public static void init() {
 
     }
+    public void run(){
 
+    }
     public static void main(String[] args) {
 
         init();
